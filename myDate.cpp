bd59c311357c1fa3e2dd9cca81e29243f46143ba
Bridget Naylor <48873843+TheCoderNamedBridget@users.noreply.github.com>
2020-09-22 09:19:59 -0700

    Update myDate.cpp

--- myDate.cpp
+++ myDate.cpp
@@ -3,15 +3,7 @@
 #include <iostream>
 using namespace std;
 
-int Greg2Julian( int mon, int day, int year )//pass m, d, y return julian
-{
-    
-}
 
-void Julian2Greg( int JD, int & month, int & day, int & year )//pass in julian date and get m, d, y through parameter list and pass by ref
-{
-    
-}
 
 MyDate::MyDate()
 {
@@ -22,6 +14,17 @@ MyDate::MyDate()
 
 MyDate::MyDate( int M, int D, int Y )
 {
+    int m1, d1, y1 = 0;
+    //Use gregorian 
+    //int JD = G2J( m, d, y );
+    //int jd2;
+    //j2g( jd, m1, y1);
+    if ( M != m1 || D != d1 || Y != y1 )
+    {
+        month = 5;
+        day = 11;
+        year = 1959;
+    }
     month = M;
     day = D;
     year = Y;
@@ -83,17 +86,27 @@ void MyDate::display()
 
 void MyDate::increaseDate( int n )
 {
-    day += n;
+    int gregDate = Greg2Julian( month, day, year ) + n;
+    Julian2Greg( gregDate, month, day, year );
+    //turn current date into a julian date add n 
+    //convert bak to gregorian date
 }
 
 void MyDate::decreaseDate( int n )
 {
-    day -= n;
+    //turn current date into a julian date add n 
+    //convert bak to gregorian date
+    int gregDate = Greg2Julian( month, day, year) - n;
+    Julian2Greg( gregDate, month, day, year );
 }
 
-int MyDate::daysBetween( MyDate D )//TODO finish this function
+int MyDate::daysBetween( MyDate D )
 {
-    return 0;
+    // turn both of the dates into julian dates then subtract 
+    //original from D
+    int gregDate1 = Greg2Julian( D.getMonth(), D.getDay(), D.getYear() );
+    int gregDate2 = Greg2Julian( month, day, year );
+    return gregDate1 - gregDate2;
 }
 
 int MyDate::getMonth()
@@ -113,10 +126,55 @@ int MyDate::getYear()
 
 int dayOfYear()//return number of days since current year began
 {
+    //use julian dates 
+    
     return 0;
 }
 
 string dayName()//return day of week, mondy, tuesday,...
 {
-    return "default";
+    //figure out today's name of the day
+    //convert to julian
+    //then use the modulo to calculate other dates day of the week
+    //9/22/2020 = tuesday
+    string dayName = "";
+    int tuesdayRemainder = Greg2Julian( 9, 22, 2020 ) % 7;
+    if ( Greg2Julian( mont, day, year ) % 7 == 0 )
+    {
+        dayName = "Tuesday";
+    }
+    else if ( Greg2Julian( mont, day, year ) % 7 == 1 )
+    {
+        dayName = "Wednesday";
+    }
+    else if ( Greg2Julian( mont, day, year ) % 7 == 2 )
+    {
+        dayName = "Thursday";
+    }
+    else if ( Greg2Julian( mont, day, year ) % 7 == 3 )
+    {
+        dayName = "Friday";
+    }
+    else if ( Greg2Julian( mont, day, year ) % 7 == 4 )
+    {
+        dayName = "Saturday";
+    }
+    else if ( Greg2Julian( mont, day, year ) % 7 == 5 )
+    {
+        dayName = "Sunday";
+    }
+    else if ( Greg2Julian( mont, day, year ) % 7 == 6 )
+    {
+        dayName = "Monday";
+    }
+    return dayName;
 }
+
+
+
+
+
+
+
+
+
