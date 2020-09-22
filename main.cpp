edbcfb3290959ff55e56121b9659a567ae87c959
Bridget Naylor <48873843+TheCoderNamedBridget@users.noreply.github.com>
2020-09-22 09:54:11 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,36 +15,7 @@ that I typed each and every line of code in this program.
 #include <iostream> 
 using namespace std;
 
-int Greg2Julian( int mon, int day, int year )//pass m, d, y return julian
-{
-    int JD = day - 32075 + 1461*(year + 4800+(mon-14)/12)/4+367*
-        (year-2-(year-14)/144)/12-3*((year+4900+(mon-14)/12)/100)/4;
-        return JD;
-}
 
-void Julian2Greg( int JD, int & month, int & day, int & year )//pass in julian date and get m, d, y through parameter list and pass by ref
-{
-    int L;
-    int N;
-    int J;
-    int K;
-    int I;
-    L = JD + 68569;
-    N = 4 * L / 146097;
-    L = L - ( 146097 * N + 3 ) / 4;
-    I = 4000 * ( L + 1 ) / 1461001;
-    L = L - 1461 * I / 4 + 31;
-    J = 80 * L / 2447;
-    K = L - 2447 * J / 80;
-    L = J / 11;
-    J = J + 2 - 12 * L;
-    I = 100 * ( N - 49 ) + I + L;
-    
-    year = I;
-    month = J;
-    day = K;
-    cout<<month<<" "<<day<<" "<<year<<endl;
-}
 
 int main()
 {
@@ -53,11 +24,23 @@ int main()
     int yearDate;
     
     MyDate date = MyDate( 1, 1, 1970 );
+    MyDate date1 = MyDate( 1, 1, 1970 );
+    MyDate date2 = MyDate( 1, 30, 1970 );
+    MyDate date3 = MyDate( 9, 21, 2020 );
     date.display();
     cout<<endl;
-    Julian2Greg( 2406842.8125, dayDate, monDate, yearDate );
-    
-    cout<<"Greg to Julian: "<<Greg2Julian( 8, 11, 1877 )<<endl;
+    date.increaseDate(50);
+    date.display();
+    cout<<endl;
+    date.decreaseDate(55);
+    date.display();
+    cout<<endl;
+    cout<<date1.daysBetween(date2);
+    cout<<endl;
+    cout<<date1.dayOfYear();
+    cout<<endl;
+    cout<<date3.dayName();
+
 
     return 0;
 }
