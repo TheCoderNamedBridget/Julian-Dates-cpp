da81d15701e7f9b7ddc45280158cb52602e6fb17
Bridget Naylor <48873843+TheCoderNamedBridget@users.noreply.github.com>
2020-09-22 09:54:43 -0700

    Update myDate.h

--- myDate.h
+++ myDate.h
@@ -1,6 +1,6 @@
 
-#ifndef MYDATE_H
-#define MYDATE_H
+#ifndef myDate_H
+#define myDate_H
 #include <iostream>
 using namespace std;
 
@@ -13,18 +13,19 @@ class MyDate
         int year;
     
     public:
-        myDate();//set date to may, 11, 1959
-        myDate( int M, int D, int Y );//set date to parameter values
+        MyDate();//set date to may, 11, 1959
+        MyDate( int M, int D, int Y );//set date to parameter values
         void display();//display date in format (month, day, year) no endl after
         void increaseDate( int N );//increment date by n days
         void decreaseDate( int N );//decrement date by n days
-        int daysBetween( myDate D );// return number of day between this date and date D. D - this date
+        int daysBetween( MyDate D );// return number of day between this date and date D. D - this date
         int getMonth();//return the month in int forma
         int getDay();//return the day of the month
         int getYear();//return the yearint dayOfYear();//return he number of days since the current year began
+        int dayOfYear();//return number of days since the year began
         string dayName();//reutrn monday, tuesday
     
     
-}
+};
 
 #endif
